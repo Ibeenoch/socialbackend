100a588a2e97a54ad35b2a86bed13e6cc3f3714f
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2022-12-13 22:20:19 +0100

    Create person.js

---
+++ models/person.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    posts:[
+           {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Post'
+       }
+   ],
+   profile:{
+     type: [ {
+   type: mongoose.Schema.Types.ObjectId,
+   ref: 'Profiles'
+} ],
+validate: [(val) => val.length <= 1, 'only one profile need'],
+},
+   followers: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }
+],
+following: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }
+],
+handle: {
+    type: String,
+},
+bio: String,
+profilepics:  {
+    url: String,
+    public_id: String,
+},
+coverphoto: {
+    url: String,
+    public_id: String,
+},
+},{
+    timestamps: true,
+})
+const User = mongoose.model('User', userSchema)
+
+export default User;
